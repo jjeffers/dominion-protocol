5a5d646e34a826e3acbbefa2506d611313115e0f
github-action-benchmark <github@users.noreply.github.com>
2026-03-30 19:44:48 +0000

    add Godot Memory Profile (customSmallerIsBetter) benchmark result for e54616c01dec9efad2d674e1f476dd2204b3d70c

---
+++ dev/bench/data.js
@@ -0,0 +1,62 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1774899888909,
+  "repoUrl": "https://github.com/jjeffers/dominion-protocol",
+  "entries": {
+    "Godot Memory Profile": [
+      {
+        "commit": {
+          "author": {
+            "email": "jdjeffers@protonmail.com",
+            "name": "James Jeffers",
+            "username": "jjeffers"
+          },
+          "committer": {
+            "email": "jdjeffers@protonmail.com",
+            "name": "James Jeffers",
+            "username": "jjeffers"
+          },
+          "distinct": true,
+          "id": "e54616c01dec9efad2d674e1f476dd2204b3d70c",
+          "message": "Clean working directory before gh-pages checkout to prevent abort",
+          "timestamp": "2026-03-30T15:37:49-04:00",
+          "tree_id": "263c253910b814e37a128be90db599dd5ab20908",
+          "url": "https://github.com/jjeffers/dominion-protocol/commit/e54616c01dec9efad2d674e1f476dd2204b3d70c"
+        },
+        "date": 1774899888062,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Host Peak Static Memory",
+            "value": 624,
+            "unit": "MB"
+          },
+          {
+            "name": "Host Peak Node Count",
+            "value": 751,
+            "unit": "Nodes"
+          },
+          {
+            "name": "Host Peak Resource Count",
+            "value": 37,
+            "unit": "Resources"
+          },
+          {
+            "name": "Client Peak Static Memory",
+            "value": 623,
+            "unit": "MB"
+          },
+          {
+            "name": "Client Peak Node Count",
+            "value": 749,
+            "unit": "Nodes"
+          },
+          {
+            "name": "Client Peak Resource Count",
+            "value": 36,
+            "unit": "Resources"
+          }
+        ]
+      }
+    ]
+  }
+}
