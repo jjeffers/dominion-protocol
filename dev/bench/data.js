731777386766fc61d27b40ec8d222771a5e33364
github-action-benchmark <github@users.noreply.github.com>
2026-03-30 19:55:34 +0000

    add Godot Memory Profile (customSmallerIsBetter) benchmark result for 2cb8a8feb5ecb5596350547a3dcf46b5ed9c502f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774899888909,
+  "lastUpdate": 1774900534916,
   "repoUrl": "https://github.com/jjeffers/dominion-protocol",
   "entries": {
     "Godot Memory Profile": [
@@ -48,6 +48,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "Client Peak Node Count",
             "value": 749,
+            "unit": "Nodes"
+          },
+          {
+            "name": "Client Peak Resource Count",
+            "value": 36,
+            "unit": "Resources"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jdjeffers@protonmail.com",
+            "name": "James Jeffers",
+            "username": "jjeffers"
+          },
+          "committer": {
+            "email": "jdjeffers@protonmail.com",
+            "name": "James Jeffers",
+            "username": "jjeffers"
+          },
+          "distinct": true,
+          "id": "2cb8a8feb5ecb5596350547a3dcf46b5ed9c502f",
+          "message": "Update gitignore rules",
+          "timestamp": "2026-03-30T15:48:21-04:00",
+          "tree_id": "88a88575e0cbade6e01f575bc98f5c3439c7f9a5",
+          "url": "https://github.com/jjeffers/dominion-protocol/commit/2cb8a8feb5ecb5596350547a3dcf46b5ed9c502f"
+        },
+        "date": 1774900534052,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Host Peak Static Memory",
+            "value": 627,
+            "unit": "MB"
+          },
+          {
+            "name": "Host Peak Node Count",
+            "value": 775,
+            "unit": "Nodes"
+          },
+          {
+            "name": "Host Peak Resource Count",
+            "value": 37,
+            "unit": "Resources"
+          },
+          {
+            "name": "Client Peak Static Memory",
+            "value": 626,
+            "unit": "MB"
+          },
+          {
+            "name": "Client Peak Node Count",
+            "value": 771,
             "unit": "Nodes"
           },
           {
