f8b02fa0152bec3cba25213a333eeef83e68faa1
github-action-benchmark <github@users.noreply.github.com>
2026-03-31 21:30:26 +0000

    add Godot Memory Profile (customSmallerIsBetter) benchmark result for 9a5200700a71bb7480b3232ca8bae2fa8c605d5f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774984272084,
+  "lastUpdate": 1774992626906,
   "repoUrl": "https://github.com/jjeffers/dominion-protocol",
   "entries": {
     "Godot Memory Profile": [
@@ -588,6 +588,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "Client Peak Node Count",
             "value": 789,
+            "unit": "Nodes"
+          },
+          {
+            "name": "Client Peak Resource Count",
+            "value": 38,
+            "unit": "Resources"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jdjeffers@protonmail.com",
+            "name": "James Jeffers",
+            "username": "jjeffers"
+          },
+          "committer": {
+            "email": "jdjeffers@protonmail.com",
+            "name": "James Jeffers",
+            "username": "jjeffers"
+          },
+          "distinct": true,
+          "id": "9a5200700a71bb7480b3232ca8bae2fa8c605d5f",
+          "message": "Fix: Regional defection alignment override (Oil Capture Bug)",
+          "timestamp": "2026-03-31T17:23:06-04:00",
+          "tree_id": "87c360174a19e031e712165c679fe2c117aadefe",
+          "url": "https://github.com/jjeffers/dominion-protocol/commit/9a5200700a71bb7480b3232ca8bae2fa8c605d5f"
+        },
+        "date": 1774992624623,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Host Peak Static Memory",
+            "value": 626,
+            "unit": "MB"
+          },
+          {
+            "name": "Host Peak Node Count",
+            "value": 807,
+            "unit": "Nodes"
+          },
+          {
+            "name": "Host Peak Resource Count",
+            "value": 39,
+            "unit": "Resources"
+          },
+          {
+            "name": "Client Peak Static Memory",
+            "value": 625,
+            "unit": "MB"
+          },
+          {
+            "name": "Client Peak Node Count",
+            "value": 805,
             "unit": "Nodes"
           },
           {
