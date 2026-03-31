502e915c06987ff473b2622a4a2e252bf1b549de
github-action-benchmark <github@users.noreply.github.com>
2026-03-31 19:11:12 +0000

    add Godot Memory Profile (customSmallerIsBetter) benchmark result for a1c8cd7f14df063349b0d7182ecf8f34952518e2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774965256556,
+  "lastUpdate": 1774984272084,
   "repoUrl": "https://github.com/jjeffers/dominion-protocol",
   "entries": {
     "Godot Memory Profile": [
@@ -539,6 +539,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "Client Peak Resource Count",
             "value": 37,
+            "unit": "Resources"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jdjeffers@protonmail.com",
+            "name": "James Jeffers",
+            "username": "jjeffers"
+          },
+          "committer": {
+            "email": "jdjeffers@protonmail.com",
+            "name": "James Jeffers",
+            "username": "jjeffers"
+          },
+          "distinct": true,
+          "id": "a1c8cd7f14df063349b0d7182ecf8f34952518e2",
+          "message": "Implement TAB and C camera navigation keybindings and overhaul KeyHintsPanel UI",
+          "timestamp": "2026-03-31T15:03:33-04:00",
+          "tree_id": "3b91f69463c373d879e996aee54bf615f9e63251",
+          "url": "https://github.com/jjeffers/dominion-protocol/commit/a1c8cd7f14df063349b0d7182ecf8f34952518e2"
+        },
+        "date": 1774984263323,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Host Peak Static Memory",
+            "value": 623,
+            "unit": "MB"
+          },
+          {
+            "name": "Host Peak Node Count",
+            "value": 791,
+            "unit": "Nodes"
+          },
+          {
+            "name": "Host Peak Resource Count",
+            "value": 39,
+            "unit": "Resources"
+          },
+          {
+            "name": "Client Peak Static Memory",
+            "value": 622,
+            "unit": "MB"
+          },
+          {
+            "name": "Client Peak Node Count",
+            "value": 789,
+            "unit": "Nodes"
+          },
+          {
+            "name": "Client Peak Resource Count",
+            "value": 38,
             "unit": "Resources"
           }
         ]
