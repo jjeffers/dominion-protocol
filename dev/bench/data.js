a8017b3661106b25899121c430b7f67b78ac58b0
github-action-benchmark <github@users.noreply.github.com>
2026-04-04 20:13:15 +0000

    add Godot Memory Profile (customSmallerIsBetter) benchmark result for 19cd68c7301827c068b47f4e04033bd6072d682b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775333449342,
+  "lastUpdate": 1775333595262,
   "repoUrl": "https://github.com/jjeffers/dominion-protocol",
   "entries": {
     "Godot Memory Profile": [
@@ -1398,6 +1398,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "Client Peak Node Count",
             "value": 850,
+            "unit": "Nodes"
+          },
+          {
+            "name": "Client Peak Resource Count",
+            "value": 38,
+            "unit": "Resources"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jdjeffers@protonmail.com",
+            "name": "James Jeffers",
+            "username": "jjeffers"
+          },
+          "committer": {
+            "email": "jdjeffers@protonmail.com",
+            "name": "James Jeffers",
+            "username": "jjeffers"
+          },
+          "distinct": true,
+          "id": "19cd68c7301827c068b47f4e04033bd6072d682b",
+          "message": "chore(version): bump version to v0.10.1",
+          "timestamp": "2026-04-04T16:03:26-04:00",
+          "tree_id": "45b8281487325761fa2736ca13992a0d628867d8",
+          "url": "https://github.com/jjeffers/dominion-protocol/commit/19cd68c7301827c068b47f4e04033bd6072d682b"
+        },
+        "date": 1775333594427,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Host Peak Static Memory",
+            "value": 778,
+            "unit": "MB"
+          },
+          {
+            "name": "Host Peak Node Count",
+            "value": 808,
+            "unit": "Nodes"
+          },
+          {
+            "name": "Host Peak Resource Count",
+            "value": 39,
+            "unit": "Resources"
+          },
+          {
+            "name": "Client Peak Static Memory",
+            "value": 779,
+            "unit": "MB"
+          },
+          {
+            "name": "Client Peak Node Count",
+            "value": 806,
             "unit": "Nodes"
           },
           {
