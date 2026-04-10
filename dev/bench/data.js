249876082ec19e4a801134208b72d1d8f205721d
github-action-benchmark <github@users.noreply.github.com>
2026-04-10 14:00:46 +0000

    add Godot Memory Profile (customSmallerIsBetter) benchmark result for 4f6e28c666286c24c7a6237d014475d1edf0df7d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775600028987,
+  "lastUpdate": 1775829646284,
   "repoUrl": "https://github.com/jjeffers/dominion-protocol",
   "entries": {
     "Godot Memory Profile": [
@@ -1830,6 +1830,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "Client Peak Node Count",
             "value": 803,
+            "unit": "Nodes"
+          },
+          {
+            "name": "Client Peak Resource Count",
+            "value": 40,
+            "unit": "Resources"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jdjeffers@protonmail.com",
+            "name": "James Jeffers",
+            "username": "jjeffers"
+          },
+          "committer": {
+            "email": "jdjeffers@protonmail.com",
+            "name": "James Jeffers",
+            "username": "jjeffers"
+          },
+          "distinct": true,
+          "id": "4f6e28c666286c24c7a6237d014475d1edf0df7d",
+          "message": "Fix naval pathfinding issues, embed Suez and Panama canals, embed Great Lakes bathemytry, fix unit interpolation bug",
+          "timestamp": "2026-04-07T19:41:33-04:00",
+          "tree_id": "72ca60f9907ad96763a49a2ee2d93ac7245a150b",
+          "url": "https://github.com/jjeffers/dominion-protocol/commit/4f6e28c666286c24c7a6237d014475d1edf0df7d"
+        },
+        "date": 1775829645761,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Host Peak Static Memory",
+            "value": 748,
+            "unit": "MB"
+          },
+          {
+            "name": "Host Peak Node Count",
+            "value": 865,
+            "unit": "Nodes"
+          },
+          {
+            "name": "Host Peak Resource Count",
+            "value": 41,
+            "unit": "Resources"
+          },
+          {
+            "name": "Client Peak Static Memory",
+            "value": 749,
+            "unit": "MB"
+          },
+          {
+            "name": "Client Peak Node Count",
+            "value": 863,
             "unit": "Nodes"
           },
           {
